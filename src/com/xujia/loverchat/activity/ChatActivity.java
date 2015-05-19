4829e1c6ef0cb840cd81234deb9063ae7f9b077b
Administrator <Administrator@PC-20130807GMQL>
2015-05-19 23:46:08 +0800

    [basic requirement]the left menu view

--- src/com/xujia/loverchat/activity/ChatActivity.java
+++ src/com/xujia/loverchat/activity/ChatActivity.java
@@ -7,14 +7,23 @@ import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.Window;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TabHost;
 
 import com.xujia.loverchat.R;
 import com.xujia.loverchat.R.layout;
 import com.xujia.loverchat.R.menu;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ChatActivity extends Activity {
 private TabHost tabHost;
+private ListView menus;
+private List<Map<String,Object>> menuList;
 private LocalActivityManager mLocalActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +34,28 @@ private LocalActivityManager mLocalActivity;
         mLocalActivity = new LocalActivityManager(this, false);
         mLocalActivity.dispatchCreate(savedInstanceState);
         initTabHost();
-        
+        menus = (ListView) findViewById(R.id.menus);
+        menuList = getData();
+        menus.setAdapter(new SimpleAdapter(this, menuList, R.layout.menu_list,new String[]{"item","image"}, new int[]{R.id.menu_text,R.id.menu_imageView1}));
     }
-
+    
+    private List<Map<String,Object>> getData()  {
+        List<Map<String,Object>> menuLists =  new ArrayList<Map<String,Object>>();
+        Map<String,Object> menu;
+        menu =  new HashMap<String, Object>();
+        menu.put("item", "设置1");
+        menu.put("image", R.drawable.ic_launcher);
+        menuLists.add(menu);
+        menu =  new HashMap<String, Object>();
+        menu.put("item", "设置2");
+        menu.put("image", R.drawable.ic_launcher);
+        menuLists.add(menu);
+        menu =  new HashMap<String, Object>();
+        menu.put("item", "设置3");
+        menu.put("image", R.drawable.ic_launcher);
+        menuLists.add(menu);                       
+        return menuLists;
+    }
     private void initTabHost()  {
         if(tabHost != null) {
           
